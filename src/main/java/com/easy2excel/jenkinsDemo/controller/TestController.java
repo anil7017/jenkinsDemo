67094b1861c367e8e29e758a163f6b66a45d86f4
anil <anilkumarsahoo17@gmail.com>
2021-03-31 19:25:12 +0530

    add request mapping

--- src/main/java/com/easy2excel/jenkinsDemo/controller/TestController.java
+++ src/main/java/com/easy2excel/jenkinsDemo/controller/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
     public String test() {
         return "app working fine";
     }
-    @GetMapping("/")
+    @GetMapping("/trs")
     public String test1() {
         return "jenkins demo app working fine 111111";
     }
